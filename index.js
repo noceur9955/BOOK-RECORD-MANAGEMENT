2b4a5faf7c368589bb3c23ac40be0cc71d3ae614
noceur9955 <akhileshkumarsingh692001@gmail.com>
2023-07-21 16:12:06 +0530

    Without database completed

--- index.js
+++ index.js
@@ -1,131 +1,29 @@
-const express=require("express");
-//json data import 
-const {users}=require('./data/users.json');
+const express = require("express");
+// importing routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
 
-const app=express();
+const app = express();
 
 const PORT = 8081;
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        message : "Server is up and running",
-    });
-});
-
-/**
- * Route/: /users
- * Method :GET
- * Description :Get all users 
- * Access: Public
- * Parameters : None
- */
-
-app.get('/users',(req,res)=>{
-    res.status(200).json({
-        success :true,
-        data :users,
-    });
-});
-
-/**
- * Route/: /users
- * Method :GET 
- * Description :Get single user by id 
- * Access: Public
- * Parameters : id
- */
-
-app.get("/users/:id",(req,res)=>{
-    const {id}=req.params;
-    const user=users.find((each)=>each.id===id);
-    if(!user){
-        return  res.status(404).json({
-            success:false,
-            message:"User not found",
-        });
-    }
-
-    return res.status(200).json({
-        success:true,
-        data:user,
-    });
-});
-
-/**
- * Route/: /users
- * Method :POST 
- * Description :Get new user 
- * Access: Public
- * Parameters : none
- */
-
-app.post("/users",(req,res)=>{
-    const {id,name,surname,email,subscriptionType,subscriptionDate}=req.body;
-
-    const user=users.find((each)=>each.id===id);
-
-    if(user){
-        return res.status(404).json({
-            success:false,
-            message:"User exists with this id",
-        });
-    }
-
-    users.push({
-        id,
-        name,
-        surname,
-        email,
-        subscriptionType,
-        subscriptionDate,
-    });
-
-    return res.status(201).json({
-        success: true,
-        data :users
-    });
-});
-
-/**
- * Route: /users/:id
- * Method: PUT
- * Description: Updating user data
- * Access: Public
- * Parameters: id
- */
-app.put("/users/:id", (req, res) => {
-    const { id } = req.params;
-    const { data } = req.body;
-  
-    const user = users.find((each) => each.id === id);
-  
-    if (!user)
-      return res.status(404).json({ success: false, message: "User not found" });
-  
-    const updatedUser = users.map((each) => {
-      if (each.id === id) {
-        return {
-          ...each,
-          ...data,
-        };
-      }
-      return each;
-    });
-  
-    return res.status(200).json({
-      success: true,
-      data: updatedUser,
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is up and running",
   });
-
-app.get("*",(req,res)=>{
-    res.status(404).json({
-        message:"This route doesnot exist",
-    });
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server is running at port ${PORT}`);
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "This route does not exist",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
 });
